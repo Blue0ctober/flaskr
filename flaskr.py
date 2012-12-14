e01bbd0263122090302037a0447790f7b42391e4
Edgar Valdes <edgargvaldes@gmail.com>
2012-12-14 00:46:58 -0800

    added some more imports and created database location

--- flaskr.py
+++ flaskr.py
@@ -1,9 +1,11 @@
 # IMPORT ALL THE THINGS!
+from __future__ import with_statement
+from contextlib import closing
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 
 # CONFIG ALL THE THINGS!
-DATABASE = '/tmp/flaskr.db'
+DATABASE = 'tmp/flaskr.db'
 DEBUT = True
 SECRET_KEY = 'secret stuff'
 USERNAME = 'admin'
