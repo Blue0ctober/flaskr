d74aa251ac8f34b3a33b18475f800057e13d2f23
Edgar Valdes <edgargvaldes@gmail.com>
2012-12-14 00:40:10 -0800

    added initial flaskr.py and enviroment for flask

---
+++ flaskr.py
@@ -0,0 +1,20 @@
+# IMPORT ALL THE THINGS!
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+
+# CONFIG ALL THE THINGS!
+DATABASE = '/tmp/flaskr.db'
+DEBUT = True
+SECRET_KEY = 'secret stuff'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+# App Starts NOW!
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+if __name__ == '__main__':
+    app.run()
