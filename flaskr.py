5ea1d6a10a04f725b3b579770608480d38942702
Edgar Valdes <edgargvaldes@gmail.com>
2012-12-14 00:50:44 -0800

    step 3: creating the databse, added def init_db()

--- flaskr.py
+++ flaskr.py
@@ -18,5 +18,11 @@ app.config.from_object(__name__)
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
+def init_db()
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
 if __name__ == '__main__':
     app.run()
